79a1bbd6e773c2ab8ad3ec47b5974fa838843d07
bobojonabdurahmonov <pubgmbobojon@gmail.com>
2025-09-16 21:06:49 +0500

    Update App.jsx and blogs.db

--- frontend/blogweb/src/App.jsx
+++ frontend/blogweb/src/App.jsx
@@ -1,12 +1,36 @@
-import Blogs from "./components/blogs"
+import { useState, useEffect } from "react";
+import Blogs from "./components/blogs";
 
 function App() {
+  const [blogblock, setBlogBLock] = useState(false);
+
+  const toBlog = () => {
+    setBlogBLock(prev => !prev);
+  };
+
+  useEffect(() => {
+    if (blogblock) {
+      document.title = "Home";
+    } else {
+      document.title = "Blogs";
+    }
+  }, [blogblock]);
+
   return (
     <div>
-      <title>Home</title>
-      <h1>Helloworld</h1>
-      <Blogs blog={"Welcome to world"}/>
+      {blogblock ? (
+        <>
+          <Blogs blog="Welcome to world" />
+          <button onClick={toBlog}>Go Home</button>
+        </>
+      ) : (
+        <>
+        <h1>Hello World</h1>
+        <button onClick={toBlog}>Go to Blogs</button>
+        </>
+      )}
     </div>
-  )}
+  );
+}
 
-export default App
+export default App;
